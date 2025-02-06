7f6f57c5ef9788eb25d50397a8bc556332997092
Farin <farinattar99@gmail.com>
2025-02-06 19:28:01 +0530

    Changed page from WhatWeDo to GamifiedGiving

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import BreakTheChains from "./pages/BreakTheChains";
-import WhatWeDo from "./pages/WhatWeDo";
+import GamifiedGiving from "./pages/GamifiedGiving";
 import DonationPage from "./pages/Donate";
 
 const App = () => {
@@ -15,7 +15,7 @@ const App = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/what-we-do" element={<WhatWeDo />} />
+        <Route path="/gamified-giving" element={<GamifiedGiving />} />
         <Route path="/donate" element={<DonationPage />} />
       </Routes>
     </div>
